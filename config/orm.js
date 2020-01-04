269a766798465b33114584b96694b2feaea4da1a
Aimee Bojorquez <abojo079@me.com>
2020-01-03 20:29:11 -0500

    server.js

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,9 @@
 const connection = require("../config/connection");
+
+
+const sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+
