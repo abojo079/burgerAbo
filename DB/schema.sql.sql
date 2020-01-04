eba06c0f69143f8b35b713c911a8f2029c96b9fe
Aimee Bojorquez <abojo079@me.com>
2020-01-03 19:07:33 -0500

    server.js

---
+++ DB/schema.sql.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+CREATE TABLE burgers(
+id INTEGER(11) PRIMARY KEY AUTO_INCREMENT NOT NULL ,
+burger_name VARCHAR(30) NOT NULL,
+devoured TINYINT(1) NOT NULL
+
+)
---
+++ DB/seeds.sql
@@ -0,0 +1,39 @@
+INSERT INTO burgers (
+burger_name,
+devoured
+)
+
+VALUE (
+'cheese burger',
+'1'
+);
+
+INSERT INTO burgers (
+burger_name,
+devoured
+)
+
+VALUE (
+'bacon cheese burger',
+'0'
+);
+
+INSERT INTO burgers (
+burger_name,
+devoured
+)
+
+VALUE (
+'turkey burger',
+'1'
+);
+
+INSERT INTO burgers (
+burger_name,
+devoured
+)
+
+VALUE (
+'Hawaiian burger',
+'0'
+);
