4f9f0df0c5029f141203111dc7f5e1a2f501efff
Aimee Bojorquez <abojo079@me.com>
2020-01-03 19:57:02 -0500

    server.js

--- server.js
+++ server.js
@@ -4,3 +4,22 @@ const mysql = require("mysql");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+//define route here
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log(`Server listening on: http://localhost:${PORT}`);
+});
