fe1bc4e27d26042493e075cc8d9df909ad188cdc
Aimee Bojorquez <abojo079@me.com>
2020-01-03 19:47:05 -0500

    server.js

--- config/connection.js
+++ config/connection.js
@@ -4,6 +4,14 @@ const connection = mysql.createConnection({
   host     : 'localhost',
   port     : '3306',
   user: 'root',
-  password : '',
+  password : 'Amang0o79**',
   database : 'burgers_db'
 });
+
+
+connection.connect(function(err){
+  if (err) throw err;
+console.log(`connected as id ${connection.threadId}`)
+
+});
+module.exports = connection.js;
