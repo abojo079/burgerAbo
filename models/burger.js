c94b23fc32236ab61dbde2b025248404d8f1b65e
Aimee Bojorquez <abojo079@me.com>
2020-01-04 20:45:25 -0500

    server.js

--- models/burger.js
+++ models/burger.js
@@ -1,30 +1,27 @@
 const orm = require("../config/orm.js");
 
-const cat = {
-  all: function(cb) {
-    orm.all("cats", function(res) {
+const burger = {
+  selectAll: function(cb) {
+    orm.all("burgers", function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("cats", cols, vals, function(res) {
+  insertOne: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
-  update: function(objColVals, condition, cb) {
-    orm.update("cats", objColVals, condition, function(res) {
+  updateOne: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   },
   delete: function(condition, cb) {
-    orm.delete("cats", condition, function(res) {
+    orm.delete("burgers", condition, function(res) {
       cb(res);
     });
   }
 };
-
-
-
 
 module.exports = burger;
