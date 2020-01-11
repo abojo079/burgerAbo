b98c6d5670c3a86febdf3402a82a50c60ad9b075
Aimee Bojorquez <abojo079@me.com>
2020-01-10 22:13:06 -0500

    server.js

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql      = require('mysql');
 const connection;
 
 if (process.env.JAWSDB_URL) {
-connection = mysql.createConnection(process.env.JAWSDB_URL);
+connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
 connection = mysql.createConnection({
 
@@ -14,7 +14,7 @@ connection = mysql.createConnection({
 
 });
 
-};
+}
 
 connection.connect();
 module.exports = connection;
